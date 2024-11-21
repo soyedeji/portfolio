8817bf937575b267e54d5e7c80ac55a16df77d7e
smtoyedeji <oyedeji.smt@gmail.com>
2024-11-21 14:08:10 -0600

    portfolio update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,12 +30,12 @@ export default function NavBar() {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
-              src="/dev-icon.svg"
+              src="/icon2.svg"
               className="mr-3 h-6 sm:h-9"
               alt="SMT Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              smt.dev
+              so.dev
             </span>
           </Link>
           <div className="flex items-center lg:order-2">
